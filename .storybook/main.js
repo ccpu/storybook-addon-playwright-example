140b48ac2bc5eb2d37e55052bf554f2f57a11853
ccpu <m.doaie@hotmail.com>
2020-09-27 12:55:42 +0330

    added storybook-addon-playwright

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,29 @@
+const { setConfig } = require("storybook-addon-playwright/configs");
+const playwright = require("playwright");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-knobs/register",
+    "storybook-addon-playwright/preset",
+    "storybook-addon-playwright/register",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+};
+
+(async () => {
+  let browser = {
+    chromium: await playwright["chromium"].launch(),
+    firefox: await playwright["firefox"].launch(),
+    webkit: await playwright["webkit"].launch(),
+  };
+  setConfig({
+    storybookEndpoint: `http://localhost:6006/`,
+    getPage: async (browserType, options) => {
+      const page = await browser[browserType].newPage(options);
+      return page;
+    },
+    afterScreenshot: async (page) => {
+      await page.close();
+    },
+  });
+})();
